3ecbd4b0b297a33701a4194f8d83526c7c946a3d
xoyulkim <110446503+xoyulkim@users.noreply.github.com>
2022-08-02 14:39:56 +0900

    Add files via upload

---
+++ python0802/02b_tt.py
@@ -0,0 +1,28 @@
+# 02b_tt.py
+# 도형 그리기
+
+import turtle as t
+
+#삼각형 그리기
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+
+# 사각형 그리기
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+
+# 원 그리기
+t.circle(50)
+
+
+        
